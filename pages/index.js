2cd5229f1e099e76387d794b0e6b1a8aeae00a87
tszkla2 <tom_sz@comcast.net>
2022-02-18 22:55:19 -0600

    Hello World

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          HELLO WORLD <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
